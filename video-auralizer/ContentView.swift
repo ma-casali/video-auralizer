24b09fb9ae7fb3eb02aa87d99700deefec677e09
ma-casali <casali.ma98@gmail.com>
2025-12-08 13:32:14 -0800

    Minimal working camera app

---
+++ video-auralizer/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  video-auralizer
+//
+//  Created by Matthew Casali on 12/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CameraView()
+            .edgesIgnoringSafeArea(.all)
+    }
+}
---
+++ video-auralizer/video_auralizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  video_auralizerApp.swift
+//  video-auralizer
+//
+//  Created by Matthew Casali on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct video_auralizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
